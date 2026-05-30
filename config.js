fb8517f68977f30e5b8e8ac74cc4e122b4c3ac9a
mralimahdi <mr.syedalimahdi1234@gmail.com>
2026-05-30 17:18:09 +0500

    Add config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// ─── PRIVATE CONFIGURATION ───────────────────────────
+// This file is gitignored — NEVER commit this file!
+// Copy this from config.example.js and fill in your keys.
+
+window._ENV = {
+    EMAILJS_SERVICE_ID:  'service_mb11txe',
+    EMAILJS_TEMPLATE_ID: 'template_nycmc2b',
+    EMAILJS_PUBLIC_KEY:  'ni9ZGKJpxP9K8IfqP',
+};
